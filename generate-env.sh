100029b070e47ac0bf1414d5a9229407b8c79994
Robin Fanchone <rofanchone@gmail.com>
2022-03-08 05:36:55 +0100

    changement nom de la variable nom serveur hôte

--- generate-env.sh
+++ generate-env.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 echo IPADDR=$(hostname -I | awk '{print $2}') > .env
-echo HOST_NAME=$(hostname) >> .env
+echo NAME_SERVER=$(hostname) >> .env
 echo DOCKER_VERSION=$(docker --version) >> .env
